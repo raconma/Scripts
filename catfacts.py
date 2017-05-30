89c2d3e2f9ab66098fb95db20b0661f926f0168e
Xiang Chen <xiang.chen.chen@alumnos.upm.es>
2017-05-30 02:41:14 +0200

    Upload catfacts.py
    This shit change your clipboard with catfacts

---
+++ catfacts.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import json
+import urllib
+import time
+import pyperclip
+
+url = "http://catfacts-api.appspot.com/api/facts"
+
+while True:
+    req = urllib.request.urlopen(url)
+    json_object = json.load(req)
+    str1 = ''.join(json_object['facts'])
+    pyperclip.copy(str1)
